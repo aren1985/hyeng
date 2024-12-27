fdfa3c0b3c5dce4bdc9f2e846ccb83a9cbf21848
aren1985 <arenh1985@gmail.com>
2024-12-27 19:19:54 +0400

    poxac

--- src/app/lessons/lesson/page.js
+++ src/app/lessons/lesson/page.js
@@ -106,9 +106,9 @@ const LessonPage = () => {
 
       <button
         onClick={nextTheme}
-        className="bg-green-500 text-white py-2 px-6 rounded mt-6 text-lg font-semibold"
+        className="bg-purple-700 hover:bg-purple-500 text-white p-3 mt-10 w-full text-lg  rounded shadow-lg font-bold border-2 border-white"
       >
-        Next Theme
+        Next
       </button>
     </div>
   );
