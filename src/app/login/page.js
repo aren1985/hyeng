ea39b00e93af93d1882d2884bf3afc722faea03c
aren1985 <arenh1985@gmail.com>
2024-12-30 16:47:37 +0400

    fixvideo

--- src/app/login/page.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import React from "react";
-
-const Loginpage = () => {
-  return <div>login page</div>;
-};
-
-export default Loginpage;
--- src/app/register/page.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import React from "react";
-
-const Regpage = () => {
-  return <div>register page</div>;
-};
-
-export default Regpage;
