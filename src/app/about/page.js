8711bc320aa9ede7e27f1d3b2718e03f68aa3856
aren1985 <arenh1985@gmail.com>
2025-03-22 14:58:00 +0400

    aass

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -9,13 +9,13 @@ const page = () => {
       <div className="relative w-full h-[315px] max-w-xl mt-4 z-5 overflow-hidden border-8 border-purple-800">
         <iframe
           width="100%"
-          height="315"
+          height="490"
           src="https://www.youtube-nocookie.com/embed/0sef9GYoLtw?controls=0&rel=0&modestbranding=1"
           title="YouTube video"
           frameBorder="0"
           allow="autoplay;  encrypted-media"
           allowFullScreen
-          className="absolute top-[-60px] left-0"
+          className="absolute top-[-100px] left-0"
         ></iframe>
       </div>
     </div>
