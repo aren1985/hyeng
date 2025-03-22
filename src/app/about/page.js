802451d0c0b8b663eee9cb2b4d944981eea42c74
aren1985 <arenh1985@gmail.com>
2025-03-22 14:55:36 +0400

    lslsl

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -9,7 +9,7 @@ const page = () => {
       <div className="relative w-full h-[315px] max-w-xl mt-4 z-5 overflow-hidden border-8 border-purple-800">
         <iframe
           width="100%"
-          height="490"
+          height="315"
           src="https://www.youtube-nocookie.com/embed/0sef9GYoLtw?controls=0&rel=0&modestbranding=1"
           title="YouTube video"
           frameBorder="0"
